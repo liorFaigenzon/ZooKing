63a3a8fdc243270797558fcf37f7c3213089d9e2
yuval <yuvalbor01@gmail.com>
2015-10-18 09:45:05 +0300

    Revert "Changed the route so it will open correctly"
    This reverts commit 94aca37cf17c02ba44638ab2b8a51ff01664e9fb.

--- ZooKing/App_Start/RouteConfig.cs
+++ ZooKing/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ZooKing
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Zoo", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
